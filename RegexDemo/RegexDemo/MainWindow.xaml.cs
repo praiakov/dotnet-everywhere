ab527ef742b66ab98ce47831225b77e029f17e6d
praiakov <adrianopraia@live.com>
2020-03-28 02:01:00 -0300

    Close

--- RegexDemo/RegexDemo/MainWindow.xaml.cs
+++ RegexDemo/RegexDemo/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace RegexDemo
             Example2 example2 = new Example2();
             example2.Show();
         }
+
+        private void Sair_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
