d54ae8f47fb5229d0961e0e6102d71aeb820c6b9
praiakov <adrianopraia@live.com>
2020-03-28 01:33:49 -0300

    RegexDemo - Example1

---
+++ RegexDemo/RegexDemo/Example1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace RegexDemo
+{
+    /// <summary>
+    /// Interaction logic for Example1.xaml
+    /// </summary>
+    public partial class Example1 : Window
+    {
+        public Example1()
+        {
+            InitializeComponent();
+        }
+
+        private void Match_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            var fonte = webClient.DownloadString("https://github.com/openfootball/england/blob/master/2019-20/1-premierleague.txt");
+
+            var rgxDiaSemana = new Regex(@"\[([a-zA-z]){3}");
+            var rgxMesDia = new Regex(@"([A-Za-z]{3}\/\d{0,2}\])");
+
+            var semanaMatch = rgxDiaSemana.Matches(fonte);
+            var mesMatch = rgxMesDia.Matches(fonte);
+
+            for (int i = 0; i < semanaMatch.Count; i++)
+            {
+                dgData.Items.Add(new Data() { DiaSemana = semanaMatch[i].Value.Replace("[",""), MesDia = mesMatch[i].Value.Replace("]","")});
+            }
+        }
+
+        public class Data
+        {
+            public string DiaSemana { get; set; }
+
+            public string MesDia { get; set; }
+
+        }
+    }
+}
---
+++ RegexDemo/RegexDemo/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace RegexDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Example1_Click(object sender, RoutedEventArgs e)
+        {
+            Example1 example1 = new Example1();
+            example1.Show();
+        }
+    }
+}
