e7945b708212a0b1794053e441245b9a247d2622
praiakov <adrianopraia@live.com>
2020-04-14 18:24:57 -0300

    ProcessStartDemo

---
+++ ProcessStartDemo/ProcessStartDemo/ProcessStartDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Management.Automation;
+
+namespace ProcessStartDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = @"";
+
+            using (var ps = PowerShell.Create())
+            {
+                var results = ps.AddScript(command).Invoke();
+
+                foreach (var item in results)
+                {
+                    System.Console.WriteLine(item.ToString());
+                }
+            }
+        }
+    }
+}
