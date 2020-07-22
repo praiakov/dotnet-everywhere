80ef57140e7ee95a5f054aff0cb9a3314c23804f
praiakov <adrianopraia@live.com>
2020-07-22 15:57:39 -0300

    Fibonacci

---
+++ Fibonacci/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static List<int> Fibonacci(int total)
+        {
+            List<int> lista = new List<int>();
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < total; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+
+                lista.Add(a);
+            }
+
+            return lista;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números para sequência de Fibonacci? ");
+            var total = int.Parse(Console.ReadLine());
+
+            var fibList = Fibonacci(total);
+
+            foreach (var item in fibList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
