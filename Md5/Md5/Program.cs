6b77d7056f3790f1ca5f390e0e5c674edb534973
praiakov <adrianopraia@live.com>
2020-11-29 17:39:03 -0300

    Md5 sample

---
+++ Md5/Md5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Md5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("##### MD5 Hash #####");
+            
+            Console.WriteLine("Digite sua senha:");
+            
+            var pass = Console.ReadLine();
+
+            Console.WriteLine($"{getMD5Hash(pass)}");
+        }
+
+        public static string getMD5Hash(string input)
+        {
+            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
