e588ff1ce7af3c5cc47bce83aceebce5649916b4
praiakov <adrianopraia@live.com>
2020-12-01 21:18:00 -0300

    ExtensionMethodDemo

---
+++ ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 9;
+
+            bool result = i.IsPair();
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public static class PairExtensions
+    {
+        public static bool IsPair(this int value)
+        {
+
+            return value % 2 == 0 ? true : false;
+
+        }
+    }
+}
