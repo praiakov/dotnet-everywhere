55ce96f801b271b9c9ef5ca689b35e21e9f8fba6
praiakov <adrianopraia@live.com>
2020-12-05 23:23:01 -0300

    Value Type and Reference Type

---
+++ ValueAndReferencesType/ValueAndReferencesType/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ValueAndReferencesType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = x;
+            y++;
+
+            Student s = new Student();
+
+            s.Name = "Bill";
+
+            var s2 = s;
+
+            ChangeName(s2);
+                
+            Console.WriteLine("Value Type: {0}", x);
+            Console.WriteLine("Value Type: {0}", y);
+            Console.WriteLine("Refence Type: {0}", s.Name);
+        }
+
+        private static void ChangeName(Student s) => s.Name = "Jobs";
+        
+    }
+
+    internal class Student
+    {        
+        public string Name { get; internal set; }
+    }
+}
