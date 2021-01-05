f7c2ddcef8bd563de49ab4df0a662ee23d4475a9
praiakov <adrianopraia@live.com>
2021-01-04 21:32:50 -0300

    ViewComponent Demo

---
+++ ViewComponentDemo/ViewComponentDemo/ViewComponents/ListaViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ViewComponentDemo.ViewComponents
+{
+    public class ListaViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(
+            string sequencia)
+        {
+            var valores = await Task.FromResult(
+                sequencia.Split(new char[] { '|' }));
+
+            return View(valores);
+        }
+    }
+}
