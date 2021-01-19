fca6b3a4dfa2bd94a6f9393b34df4466d928577b
praiakov <adrianopraia@live.com>
2021-01-19 08:15:28 -0300

    Facade

---
+++ DesignPattern/Facade/Facade/CarFacade.cs
@@ -0,0 +1,25 @@
+﻿using Facade.Model;
+
+namespace Facade.Facade
+{
+    public class CarFacade
+    {
+        public CarModel CarModel { get; set; }
+        public CarBody CarBody { get; set; }
+        public CarAcessories CarAcessories { get; set; }
+
+        public CarFacade()
+        {
+            CarModel = new CarModel();
+            CarBody = new CarBody();
+            CarAcessories = new CarAcessories();
+        }
+
+        public void CreateCompleteCar()
+        {
+            CarModel.SetModel();
+            CarBody.SetBody();
+            CarAcessories.SetAcessories();
+        }
+    }
+}
---
+++ DesignPattern/Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿using Facade.Facade;
+using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var facade = new CarFacade();
+            facade.CreateCompleteCar();
+
+            Console.ReadKey();
+        }
+    }
+}
